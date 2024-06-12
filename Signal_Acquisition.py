59e5be86934ac33a5e52cebf5ca8afcc40f97457
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2024-06-12 15:02:42 -0400

    commented out code not to be tested yet

--- Signal_Acquisition.py
+++ Signal_Acquisition.py
@@ -14,16 +14,16 @@ sig_acq.blink_led(10)
 
 # ADC sampling clock generated on signal acquisition hat and not shared
 # sample rate ~1kHz (rounded to 51.2 kHz / 51)
-sig_acq.a_in_clock_config_write(daq.LOCAL, 1000) 
-print(sig_acq.a_in_clock_config_read())
-print(sig_acq.a_in_scan_actual_rate())
+# sig_acq.a_in_clock_config_write(daq.LOCAL, 1000) 
+# print(sig_acq.a_in_clock_config_read())
+# print(sig_acq.a_in_scan_actual_rate())
 
 ########################################################################
 # Signal acquisition
 
-sig_acq.a_in_scan_start()
-scan_data = sig_acq.a_in_scan_read()
-sig_acq.a_in_scan_cleanup()
+# sig_acq.a_in_scan_start()
+# scan_data = sig_acq.a_in_scan_read()
+# sig_acq.a_in_scan_cleanup()
 
 
 ########################################################################
