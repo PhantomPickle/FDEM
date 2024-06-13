20cc6104c36aff2517f4df91fe90f980523c5ff1
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2024-06-13 12:26:26 -0400

    boards now detectable but initialization not working

--- Signal_Acquisition.py
+++ Signal_Acquisition.py
@@ -1,16 +1,16 @@
 import daqhats as daq
 
-daq.hat_list()
+print(daq.hat_list())
 
 #######################################################################
 # Initializing DAQ hats
 
 # MCC 152 DAQ hat used for signal generation
-sig_gen = daq.mcc152()
+#sig_gen = daq.mcc152()
 
 # MCC 172 DAQ hat used for signal acquisition
-sig_acq = daq.mcc172() 
-sig_acq.blink_led(10)
+#sig_acq = daq.mcc172() 
+#sig_acq.blink_led(10)
 
 # ADC sampling clock generated on signal acquisition hat and not shared
 # sample rate ~1kHz (rounded to 51.2 kHz / 51)
