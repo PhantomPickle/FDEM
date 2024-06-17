8f0bd682bd73f65631808d10cdfcdc818fd3675b
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2024-06-17 17:16:01 -0400

    adapted finite_scan example script into signal_acquisition script

--- signal_acquisition.py
+++ signal_acquisition.py
@@ -1,29 +1,100 @@
-import daqhats as daq
+from time import sleep
+from sys import stdout, version_info
+from daqhats import mcc172, OptionFlags, SourceType, HatIDs, HatError
+import numpy as np
 
-print(daq.hat_list())
+def main(): # pylint: disable=too-many-locals, too-many-statements
 
-#######################################################################
-# Initializing DAQ hats
+    num_samples = 10000
+    scan_rate = 1000.
+    options = OptionFlags.DEFAULT
 
-# MCC 152 DAQ hat used for signal generation
-sig_gen = daq.mcc152(address=1)
+    try:
 
-# MCC 172 DAQ hat used for signal acquisition
-sig_acq = daq.mcc172() 
-sig_acq.blink_led(10)
+        hat = mcc172()
 
-# ADC sampling clock generated on signal acquisition hat and not shared
-# sample rate ~1kHz (rounded to 51.2 kHz / 51)
-# sig_acq.a_in_clock_config_write(daq.LOCAL, 1000) 
-# print(sig_acq.a_in_clock_config_read())
-# print(sig_acq.a_in_scan_actual_rate())
+        # Configure the clock and wait for sync to complete.
+        hat.a_in_clock_config_write(SourceType.LOCAL, scan_rate)
 
-########################################################################
-# Signal acquisition
+        synced = False
+        while not synced:
+            (_source_type, actual_scan_rate, synced) = hat.a_in_clock_config_read()
+            if not synced:
+                sleep(0.005)
 
-# sig_acq.a_in_scan_start()
-# scan_data = sig_acq.a_in_scan_read()
-# sig_acq.a_in_scan_cleanup()
+        input('\nPress ENTER to begin scan ...')
+    
+        hat.a_in_scan_start(0x01, num_samples, options)
+
+        print('Starting scan ... Press Ctrl-C to stop\n')
+
+        try:
+            read_and_store_data(hat, num_samples)
+        except KeyboardInterrupt:
+            hat.a_in_scan_stop()
+
+        hat.a_in_scan_cleanup()
+
+    except (HatError, ValueError) as err:
+        print('\n', err)
+
+def read_and_store_data(hat, num_samples_per_channel):
+    """
+    Reads data from the DAQ HAT and stores the data in a csv file.  
+    The reads are executed in a loop that continues until either 
+    the scan completes or an overrun error is detected.
+
+    Args:
+        hat (mcc172): The mcc172 HAT device object.
+        samples_per_channel: The number of samples to read for each channel.
+
+    Returns:
+        None
+    """
+
+    total_samples_read = 0
+    read_request_size = 1000
+    timeout = 5.0
+    scan_data = np.zeros(num_samples_per_channel)
+
+    # Since the read_request_size is set to a specific value, a_in_scan_read()
+    # will block until that many samples are available or the timeout is
+    # exceeded.
+
+    # Continuously reads data until Ctrl-C is
+    # pressed or the number of samples requested has been read.
+    while total_samples_read < num_samples_per_channel:
+        read_result = hat.a_in_scan_read(read_request_size, timeout)
+
+        # Check for an overrun error
+        if read_result.hardware_overrun:
+            print('\n\nHardware overrun\n')
+            break
+        elif read_result.buffer_overrun:
+            print('\n\nBuffer overrun\n')
+            break
+
+        samples_read = len(read_result.data)
+        total_samples_read += samples_read
+
+        print('\r{:12}'.format(samples_read),
+              ' {:12}'.format(total_samples_read), end='')
+
+        # Stores the current chunk of data
+        start_index = samples_read - read_request_size
+        stop_index = samples_read
+        scan_data[start_index:stop_index] = read_result.data
+
+    # Exports scan data to a csv
+    logname = "scan.csv"
+    logfile = open(logname, "w")
+    logfile.write("Index, Voltage (V)\n")
+    for i, voltage in enumerate(scan_data):
+        logfile.write(f"{i}, {voltage:.2f}", )
+
+    print('\n')
+    print('Data exported to CSV.')
 
 
-########################################################################
+if __name__ == '__main__':
+    main()
