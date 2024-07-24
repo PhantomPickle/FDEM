2afe086f8ad7fe0c6aaba614e0e58b6dc7ccb847
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2024-07-24 16:09:20 -0400

    testing max analog write rate

--- voltage_output.py
+++ voltage_output.py
@@ -6,20 +6,27 @@ from daqhats import mcc152, OptionFlags, SourceType, HatIDs
 import numpy as np
 
 def output_signal(hat, amplitude = .1, offset = 0, num_periods = 10):
+    one_period_vals = np.array([amplitude * np.sin(i*(2*np.pi/360)) + offset for i in range(360)])
+    full_vals = np.array([])
     for i in range(num_periods):
-        for i in range(360):
-            output = amplitude * np.sine(i*(2*np.pi/360)) + offset
-            hat.a_out_write(channel = 0, value = output, options = OptionFlags.DEFAULT)
-        # Is this going to be adding an extra point redundantly at beginning/end of each period?
+        full_vals = np.append(full_vals, one_period_vals)
+    for i, val in enumerate(full_vals):
+        hat.a_out_write(channel = 0, value = val, options = OptionFlags.DEFAULT)
+    # Is this going to be adding an extra point redundantly at beginning/end of each period?
 
 def main():
 
-    output_hat = mcc152(1, num_periods = 1000)
+    output_hat = mcc152(1)
 
     input('\nPress ENTER to begin output ...')
     print('Starting output ... Press Ctrl-C to stop\n')
 
-    output_signal(output_hat)
+    while(True): 
+        output_hat.a_out_write(channel = 0, value = 0, options = OptionFlags.DEFAULT)
+        output_hat.a_out_write(channel = 0, value = 5, options = OptionFlags.DEFAULT)
+
+    #output_signal(output_hat, amplitude = 1, offset = 1, num_periods = 1000)
+    output_hat.a_out_write(channel = 0, value = 0, options = OptionFlags.DEFAULT) # sets output back to 0
 
 if __name__ == '__main__':
     main()
