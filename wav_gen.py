2c3118e5b7e6829c9f03edfa48c1f3acdd7ee53f
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2024-08-01 14:14:24 -0400

    changed sampling rate

--- wav_gen.py
+++ wav_gen.py
@@ -1,13 +1,12 @@
 import numpy as np
 import wave as wav
 import struct
-#from scipy  signal
-#from scipy.io  wavfile
-# matplotlib.pyplot as plt
+#from scipy.io import wavfile
+#import matplotlib.pyplot as plt
 
 
-sample_rate = 1000. # in [Hz]
-duration = 100. # in [s]
+sample_rate = 2000. # in [Hz]
+duration = 10. # in [s]
 frequency = 100. # in [Hz]
 num_frames = int(duration*sample_rate)
 
@@ -21,19 +20,12 @@ values = [int(amplitude * np.sin(i*(2*np.pi/360))) for i in range(num_frames)]
 #print(values)
 
 for i, value in enumerate(values):
-#for i in range(num_frames):
-#    value =  random.randint(-32767, 32767)
     data = struct.pack('<h', value) # 16 bit bc 'h' is short int apparently
     wav_obj.writeframesraw(data)
 
 wav_obj.close()
 
-#sample_rate, samples = wavfile.read('output.wav')
-#plt.plot(samples)
-#plt.show()
-
-# wav_obj = wav.open('output.wav', 'rb')
-# print(wav_obj.getsampwidth())
-# data_buffer = wav_obj.readframes(num_frames)
-# print(data_buffer)
-# data = struct.unpack('<h', wav_obj.readframes(num_frames))
+# sample_rate, samples = wavfile.read('output.wav')
+# print(samples)
+# plt.plot(samples, '.')
+# plt.show()
