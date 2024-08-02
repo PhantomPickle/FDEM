931734e0ea6ef59cc7fd5db97fc110867e442fde
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2024-08-02 13:11:11 -0400

    trying to get commands to run simultaneously

--- scan.sh
+++ scan.sh
@@ -1,4 +1,3 @@
-
-sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/signal_acquisition.py
-aplay ~/Documents/FDEM/output.wav
+aplay ~/Documents/FDEM/output.wav & sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/signal_acquisition.py
 rclone copy ~/Documents/FDEM/data onedrive:Documents/Academic/Coding/FDEM/datas
+echo CSV copied to OneDrive
