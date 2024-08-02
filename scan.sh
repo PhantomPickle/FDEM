efda5e56bce92bd5778acbbce669fc0de62a4668
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2024-08-02 13:05:40 -0400

    created shell script to automate runs

---
+++ scan.sh
@@ -0,0 +1,4 @@
+
+sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/signal_acquisition.py
+aplay ~/Documents/FDEM/output.wav
+rclone copy ~/Documents/FDEM/data onedrive:Documents/Academic/Coding/FDEM/datas
