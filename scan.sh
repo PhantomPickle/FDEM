43ef36b288e3ecb171eab64b76cadda9122682ff
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2024-08-02 13:21:30 -0400

    fixed typo

--- scan.sh
+++ scan.sh
@@ -1,3 +1,3 @@
 aplay ~/Documents/FDEM/output.wav & sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/signal_acquisition.py
-rclone copy ~/Documents/FDEM/data onedrive:Documents/Academic/Coding/FDEM/datas
+rclone copy ~/Documents/FDEM/data onedrive:Documents/Academic/Coding/FDEM/data
 echo CSV copied to OneDrive
