93b80a19e07fb39bee579395d53b3c45f7fdf600
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2024-11-18 13:50:41 -0500

    recording system time for gps and mag data

--- GPS.py
+++ GPS.py
@@ -1,17 +1,24 @@
 import serial
+import datetime
+import os
 
 from ublox_gps import UbloxGps
 
 port = serial.Serial('/dev/ttyACM0', baudrate=38400, timeout=1)
 gps = UbloxGps(port)
 
+gps_times = []
+gps_coords = []
 def run():
-
+    # Acquires gps times and coordinates
     try:
         print("Listening for UBX Messages")
         while True:
             try:
+                time = datetime.now().second
                 geo = gps.geo_coords()
+                gps_times. append(time)
+                gps_coords.append(geo)
                 print("Longitude: ", geo.lon) 
                 print("Latitude: ", geo.lat)
                 print("Heading of Motion: ", geo.headMot)
@@ -21,6 +28,15 @@ def run():
     finally:
         port.close()
 
+def export():
+    # Exports gps data to a csv
+    logname = "gps.csv"
+    path = os.path.expanduser('~apa/Documents/FDEM/data/'+logname)
+    logfile = open(path, "w")
+    logfile.write("Times (s), Voltage (V)\n")
+    for i in range(len(gps_times)):
+        logfile.write(f"{gps_times[i]}, {gps_coords[i]:.7f}\n")
 
 if __name__ == '__main__':
     run()
+    export()
--- scan.sh
+++ scan.sh
@@ -1,3 +1,3 @@
-aplay ~/Documents/FDEM/output.wav & sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/signal_acquisition.py
+aplay ~/Documents/FDEM/output.wav & sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/signal_acquisition.py & sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/gps.py
 rclone copy ~/Documents/FDEM/data onedrive:Documents/Academic/Coding/FDEM/data
-echo CSV copied to OneDrive.
+echo CSVs copied to OneDrive.
