465af54d802137faabb63d2d5e3294d2b429c15c
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2025-02-28 16:28:45 -0500

    fixed time logging

--- signal_acquisition.py
+++ signal_acquisition.py
@@ -115,16 +115,13 @@ def export(scan_data, start_time, scan_rate):
     scan_rate: sampling frequency of the DAQ
     '''
     
-    scan_times = [(start_time/scan_rate)*i for i in range(len(scan_data['Channel 1']))]
+    scan_times = [start_time+(scan_rate*i) for i in range(len(scan_data['Channel 1']))]
 
     logname = "mag_data.csv"
     path = os.path.expanduser('~apa/Documents/FDEM/data/'+logname)
     logfile = open(path, "w")
     print("Writing mag data to log file.")
     logfile.write("Times (s), Ch 1 Voltage (V), Ch 2 Voltage (V)\n")
-    print(len(scan_times))
-    print(len(scan_data['Channel 1']))
-    print(len(scan_data['Channel 2']))
     for i in range(len(scan_data['Channel 1'])):
         logfile.write(f"{scan_times[i]},{scan_data['Channel 1'][i]:.7f},{scan_data['Channel 2'][i]:.7f}\n")
     logfile.close()
