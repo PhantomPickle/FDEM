fc2fcfe02b37774178aa20b9a0cb457467d87c74
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2025-03-16 14:02:31 -0400

    finished autocorrelation code and added spectrogram plotting

--- utilities/signal_analysis_methods.py
+++ utilities/signal_analysis_methods.py
@@ -1,18 +1,15 @@
 import numpy as np
 
-def autocorrelation(time_series, lag=1):
+def autocorrelation(time_series, lag_interval=0):
     
     mean = sum(time_series)/len(time_series)
     variance = sum((time_series-mean)**2)/len(time_series)
-    print(variance)
     
-    lagged_auto_covariances = []
-    for i in range(len(time_series)-lag):
-        k = i+lag
-        #print(time_series[i+1:]-mean)
-        #print('lac', sum((time_series[k:]-mean)*(time_series[k-1:]-mean))/len(time_series))
-        lagged_auto_covariances.append(sum((time_series[i+1:]-mean)*(time_series[i:]-mean))/len(time_series))
+    lagged_auto_covariances = np.zeros(len(time_series)-lag_interval)
+    print(lagged_auto_covariances)
+    for i in range(len(time_series)-lag_interval):
+        k = i+lag_interval # lag
+        lagged_auto_covariances[i] = (sum((time_series[:-k]-mean)*(time_series[k:]-mean))/len(time_series))
       
-    print('lac list', lagged_auto_covariances)  
     autocorrelations = np.array(lagged_auto_covariances)/variance
     return autocorrelations
