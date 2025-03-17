384080e2cf3f1afa99c4ab1eca4afbde48fe249e
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2025-03-17 12:07:09 -0400

    commented out unnecessary bit in autocorrelation function

--- utilities/signal_analysis_methods.py
+++ utilities/signal_analysis_methods.py
@@ -3,12 +3,12 @@ import numpy as np
 def autocorrelation(time_series, lag_interval=0):
     
     mean = sum(time_series)/len(time_series)
-    variance = sum((time_series-mean)**2)/len(time_series)
+    variance = sum((time_series-mean)**2) #/len(time_series) normally, but cancels with factor later.
     
     lagged_auto_covariances = np.zeros(len(time_series)-lag_interval)
     for i in range(len(time_series)-lag_interval):
         k = i+lag_interval # lag
-        lagged_auto_covariances[i] = (sum((time_series[:-k]-mean)*(time_series[k:]-mean))/len(time_series))
+        lagged_auto_covariances[i] = sum((time_series[:-k]-mean)*(time_series[k:]-mean)) #/len(time_series))
       
     autocorrelations = np.array(lagged_auto_covariances)/variance
     return autocorrelations
