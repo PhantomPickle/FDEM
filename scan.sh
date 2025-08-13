df0cfcd48ac4be6cdd4671ec79a17460b2b2d0c9
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2025-08-13 11:28:50 -0400

    field changes

--- scan.sh
+++ scan.sh
@@ -1,3 +1,3 @@
-aplay ~/Documents/FDEM/output_waveform.wav & sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/signal_acquisition.py & sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/gps.py
+aplay ~/Documents/FDEM/output_waveform_chirp.wav & sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/signal_acquisition.py & sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/gps.py
 rclone copy ~/Documents/FDEM/data onedrive:Documents/Academic/Coding/FDEM/data
 echo CSVs copied to OneDrive.
