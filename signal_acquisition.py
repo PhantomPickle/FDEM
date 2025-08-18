7b0918fe9220696214e3af3f6a1ab8f206d8cc93
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2025-08-18 16:48:24 -0400

    debugging secondary coil acquisition -- stripping outputs

--- signal_acquisition.py
+++ signal_acquisition.py
@@ -133,6 +133,7 @@ def read_and_store_data(hats, num_samples_per_channel, t0, channels):
                                      total_samples_read_per_channel[i]] = read_result.data
             if i == 1:
                 print(read_result.data[0])
+                print(read_result.shape)
                 scan_data['Ch 1'][total_samples_read_per_channel[i]-samples_read_per_channel[i]:\
                                      total_samples_read_per_channel[i]] = read_result.data[0]
                 scan_data['Ch 2'][total_samples_read_per_channel[i]-samples_read_per_channel[i]:\
