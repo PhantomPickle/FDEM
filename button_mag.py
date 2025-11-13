8dae0be6681e3d64ea18376664dadb17205b15b9
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2025-11-13 16:31:24 -0500

    parsed x,y,z mag data in MEDA log file

--- button_mag.py
+++ button_mag.py
@@ -33,7 +33,7 @@ with open ("data/MagData-"+ str(string_time)+".csv","w") as file:
 		try:
 			mag_data_raw=ser.readline()
 			current_time = get_seconds()
-			mag_data_parsed = re.findall(r'[+-]\d{6}', mag_data_raw)
+			mag_data_parsed = re.findall(rb'[+-]\d{6}', mag_data_raw)
 			file.write(f"{current_time}, {mag_data_parsed[0]}, {mag_data_parsed[1]}, {mag_data_parsed[2]}\n"),
 			print(mag_data_raw),
 		except KeyboardInterrupt:
