21cc82a17a9a5cac9364142c0122f90f7172dd5a
Alexandra Pick-Aluas <alex@Selune.local>
2026-03-31 18:10:15 -0400

    added interrupt to bash script

--- scan.sh
+++ scan.sh
@@ -1,7 +1,20 @@
+cleanup() {
+    echo "Caught interrupt, terminating processes..."
+    kill $(jobs -p)
+    exit 0
+}
+
+trap cleanup SIGINT
+
 echo "Enter scan duration:"
 read USERVAL
-aplay ~/Documents/FDEM/output_waveform_pure.wav \
-& sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/signal_acquisition.py "$USERVAL" \
-& sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/gps.py "$USERVAL"
+
+aplay ~/Documents/FDEM/output_waveform_pure.wav &
+sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/signal_acquisition.py "$USERVAL" &
+sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/gps.py "$USERVAL" &
+
+# Wait for all background processes to complete
+wait
+
 rclone copy ~/Documents/FDEM/data onedrive:Documents/Academic/Coding/FDEM/data
 echo CSVs copied to OneDrive.
