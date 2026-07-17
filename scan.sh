07197bd6ba1765704cd2b62dd3fd3be18d8eecb4
Alexandra Pick-Aluas <alex.pick.aluas@gmail.com>
2026-07-17 15:53:56 -0400

    change name of output waveform

--- scan.sh
+++ scan.sh
@@ -12,7 +12,7 @@ trap cleanup SIGINT
 echo "Enter scan duration:"
 read USERVAL
 
-aplay ~/Documents/FDEM/output_waveform_pure.wav &
+aplay ~/Documents/FDEM/test_1kHz.wav &
 sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/signal_acquisition.py "$USERVAL" &
 sudo ~/Documents/FDEM_venv/bin/python ~/Documents/FDEM/gps.py "$USERVAL" &
 
